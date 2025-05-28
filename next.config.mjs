045a2ead59e3bd000fcb3966f0a0d15f25ea010f
GabrielMCC02020 <gabrielcondori@upeu.edu.pe>
2025-05-28 18:03:11 -0500

    imagen de perfil actualizado

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['lh3.googleusercontent.com, avatars.githubusercontent.com, cdn.discordapp.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                pathname: '/**',
+            },
+        ],
     }
 };
 
 export default nextConfig;
+
+
+
